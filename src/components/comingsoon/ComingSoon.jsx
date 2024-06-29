2299ab2fd229f4444fd3dae88bec0bd04d764e33
Frank Francione <frankiefrancione@gmail.com>
2024-06-29 14:18:13 -0500

    rotated the plane

--- src/components/comingsoon/ComingSoon.jsx
+++ src/components/comingsoon/ComingSoon.jsx
@@ -1,7 +1,9 @@
 import React, { useRef, useState } from "react";
 import "./comingsoon.css";
-import { Canvas, useFrame } from "@react-three/fiber";
+import { Canvas, useFrame, extend } from "@react-three/fiber";
+import { FontLoader, TextGeometry } from "three/examples/jsm/Addons.js";
 import { OrbitControls } from "@react-three/drei";
+import { DoubleSide } from "three";
 
 const Pedestal = ({ position, size, color }) => {
   return (
@@ -12,12 +14,32 @@ const Pedestal = ({ position, size, color }) => {
   );
 };
 
+const Plane = ({ position, size, color }) => {
+  return (
+    <mesh position={position} rotation-x={-Math.PI * 0.5}>
+      <planeGeometry args={size} />
+      <meshStandardMaterial color="violet" side={DoubleSide} />
+    </mesh>
+  );
+};
+
+const Text = ({ position, size }) => {
+  return (
+    <mesh position={position}>
+      <TextGeometry args={size} />
+      <meshStandardMaterial color="violet" side={DoubleSide} />
+    </mesh>
+  );
+};
+
 const ComingSoon = () => {
   return (
-    <Canvas camera={{ position: [0, 0, 17] }}>
-      <directionalLight position={[0, 0, 2]} intensity={0.3} />
+    <Canvas camera={{ position: [0, 7, 17] }}>
+      <directionalLight position={[2, 10, 2]} intensity={0.8} />
       <ambientLight intensity={1} />
+      {/* Remember that shapes originate at the center from their position */}
       <Pedestal position={[0, 0, 0]} size={[2, 2, 5, 20]} />
+      <Plane position={[0, -2.5, 0]} size={[15, 15]} />
       <OrbitControls makeDefault={true} />
     </Canvas>
   );
