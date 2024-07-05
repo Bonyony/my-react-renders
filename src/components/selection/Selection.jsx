71ec41a6fcba0db7aed3e684539ef3e54e1969db
Frank Francione <frankiefrancione@gmail.com>
2024-07-05 11:23:23 -0500

    laid out the selection pages

--- src/components/selection/Selection.jsx
+++ src/components/selection/Selection.jsx
@@ -5,20 +5,54 @@ const Selection = () => {
   return (
     <div className="m-4 p-4">
       <h1 className="text-6xl font-bold text-center">
-        Select a render to view
+        Select a Render to View
       </h1>
-      <div className="grid" id="render_card">
-        <ul className="flex items-center gap-4">
-          <li>
-            <Link to="/renders/logo">React Logo</Link>
-          </li>
-          <li>
-            <Link to="/renders/3dtext">3D Text</Link>
-          </li>
-          <li>
-            <Link to="/renders/comingsoon">Coming Soon</Link>
-          </li>
-        </ul>
+
+      <div className="grid grid-cols-3 gap-8 my-8 mx-16">
+        <Link
+          to="/renders/logo"
+          className="text-center border-slate-800 border-2 py-5 gap-3 text-3xl font-bold hover:bg-blue-900 hover:text-white transition-all duration-200"
+        >
+          React Logo
+          <p className="text-sm">My idea for a 3D version of the React Logo</p>
+        </Link>
+
+        <Link
+          to="/renders/3dtext"
+          className="text-center border-slate-800 border-2 py-5 gap-3 text-3xl font-bold hover:bg-blue-900 hover:text-white transition-all duration-200"
+        >
+          3D Text
+          <p className="text-sm">My name floating in a HDR image</p>
+        </Link>
+
+        <Link
+          to="/renders/comingsoon"
+          className="text-center border-slate-800 border-2 py-5 gap-3 text-3xl font-bold hover:bg-blue-900 hover:text-white transition-all duration-200"
+        >
+          Coming Soon
+          <p className="text-sm">For when there is more to be done</p>
+        </Link>
+
+        <Link
+          to=""
+          className="text-center border-slate-800 border-2 py-5 gap-3 text-3xl font-bold hover:bg-blue-900 hover:text-white transition-all duration-200"
+        >
+          Nothing Yet...
+        </Link>
+
+        <Link
+          to=""
+          className="text-center border-slate-800 border-2 py-5 gap-3 text-3xl font-bold hover:bg-blue-900 hover:text-white transition-all duration-200"
+        >
+          Nothing Yet...
+        </Link>
+
+        <Link
+          to=""
+          className="text-center border-slate-800 border-2 py-5 gap-3 text-3xl font-bold hover:bg-blue-900 hover:text-white transition-all duration-200"
+        >
+          Nothing Yet...
+        </Link>
       </div>
     </div>
   );
--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -2,30 +2,32 @@ import React from "react";
 
 const AboutMe = () => {
   return (
-    <div className="m-4 p-4 flex flex-col">
-      <h1 className="flex justify-center font-bold text-center text-6xl">
-        About Me, <br />
-        Frank Francione
-      </h1>
+    <div className="m-4 p-4 flex flex-row">
+      <div className="flex flex-col">
+        <h1 className="flex font-bold text-left text-7xl">
+          About Me, <br />
+          Frank Francione
+        </h1>
 
-      <p className="flex flex-row justify-center text-xl">
-        Check out my&nbsp;
-        <a
-          className="text-blue-700 hover:text-blue-900 relative w-fit block after:block after:content-[''] after:absolute after:h-[3px] after:bg-blue-700 after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-left"
-          href="https://github.com/Bonyony"
-          target="_blank"
-        >
-          GitHub
-        </a>
-      </p>
-      <p className="flex justify-center text-xl text-center">
-        I am a Front-end / React Developer with a specialization in 3D
-        rendering. <br />
-        These are projects which reflect things I am passionate about,
-        interested in, or maybe I just think it's cool.
-      </p>
+        <p className="flex flex-row my-4 font-bold text-3xl">
+          Check out my&nbsp;
+          <a
+            className="text-blue-700 hover:text-blue-900 relative w-fit block after:block after:content-[''] after:absolute after:h-[3px] after:bg-blue-700 after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-left"
+            href="https://github.com/Bonyony"
+            target="_blank"
+          >
+            GitHub
+          </a>
+        </p>
+        <p className="flex my-4 text-xl ">
+          I am a Front-end / React Developer with a specialization in 3D
+          rendering. <br />
+          These are projects which reflect things I am passionate about,
+          interested in, or maybe I just think it's cool.
+        </p>
+      </div>
       <img
-        className="w-auto h-auto"
+        className="w-96 h-96 m-4"
         src="public/img/lookingGlass.jpeg"
         alt="My Art"
       />
