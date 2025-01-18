d1a04498cd46271103149904efc29e7a3c6b3dfa
Frank Francione <frankiefrancione@gmail.com>
2025-01-17 19:06:18 -0600

    made the UI very minimal for now

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,65 +2,88 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
 
+const SmallMenu = () => {
+  return (
+    <ul
+      tabIndex={0}
+      className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1]  w-auto p-2 shadow"
+    >
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/renders">Renders</Link>
+      </li>
+      <li>
+        <Link to="/aboutme">About</Link>
+      </li>
+    </ul>
+  );
+};
+
 const Menu = () => {
   return (
-    <>
-      <ul className="flex items-center gap-7 mt-2 sm:mt-auto">
-        <Link
-          to="/"
-          className="group p-2 bg-inherit border-2 border-sky-500 hover:bg-sky-500 text-sky-600 transition-all duration-0 rounded-sm"
-        >
-          <li className="group-hover:text-white relative w-fit block after:block after:content-[''] after:absolute after:h-[3px] after:bg-white after:w-full after:scale-x-0 after:group-hover:scale-x-100 after:transition after:duration-300 after:origin-left">
-            Home
-          </li>
+    <ul tabIndex={0} className="flex flex-row gap-5 z-[1] w-auto p-2">
+      <li>
+        <Link className="btn btn-ghost" to="/">
+          Home
         </Link>
-        <Link
-          to="/renders"
-          className="group p-2 bg-inherit border-2 border-orange-500 hover:bg-orange-500 text-orange-600 transition-all duration-0 rounded-sm"
-        >
-          <li className="group-hover:text-white relative w-fit block after:block after:content-[''] after:absolute after:h-[3px] after:bg-white after:w-full after:scale-x-0 after:group-hover:scale-x-100 after:transition after:duration-300 after:origin-left">
-            Renders
-          </li>
+      </li>
+      <li>
+        <Link className="btn btn-ghost" to="/renders">
+          Renders
         </Link>
-        <Link
-          to="/aboutme"
-          className="group p-2 bg-inherit border-2 border-emerald-500 hover:bg-emerald-500 text-emerald-600 transition-all duration-0 rounded-sm"
-        >
-          <li className="group-hover:text-white relative w-fit block after:block after:content-[''] after:absolute after:h-[3px] after:bg-white after:w-full after:scale-x-0 after:group-hover:scale-x-100 after:transition after:duration-300 after:origin-left">
-            About Me
-          </li>
+      </li>
+      <li>
+        <Link className="btn btn-ghost" to="/aboutme">
+          About
         </Link>
-      </ul>
-    </>
+      </li>
+    </ul>
   );
 };
 
 const Navbar = () => {
   return (
-    <div className="render__navbar text-black font-extrabold w-full border-b-2 border-sky-500">
-      <div className="render__links-container flex sm:flex-row flex-col  justify-between items-center p-5 mx-auto">
-        <div className="render__logo text-3xl ml2 ">
-          <h1 className="bungee uppercase">
-            <div>
-              <motion.span className="text-sky-900">R</motion.span>
-              <motion.span className="text-sky-800">e</motion.span>
-              <motion.span className="text-sky-700">a</motion.span>
-              <motion.span className="text-sky-600">c</motion.span>
-              <motion.span className="text-sky-500">t </motion.span>
-              <motion.span className="text-sky-900">R</motion.span>
-              <motion.span className="text-sky-800">e</motion.span>
-              <motion.span className="text-sky-700">n</motion.span>
-              <motion.span className="text-sky-600">d</motion.span>
-              <motion.span className="text-sky-500">e</motion.span>
-              <motion.span className="text-sky-400">r</motion.span>
-              <motion.span className="text-sky-400">s</motion.span>
-            </div>
-          </h1>
-        </div>
-        <Menu />
+    <div className="navbar px-4">
+      <div className="flex-1">
+        <h1 className=" text-xl font-black">Rendering Showcase</h1>
+      </div>
+      <div className="flex-none">
+        <nav className="dropdown dropdown-hover dropdown-end md:hidden">
+          <div tabIndex={0} role="button" className="btn btn-ghost btn-square">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-10 w-10"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h16M4 18h7"
+              />
+            </svg>
+          </div>
+          <SmallMenu />
+        </nav>
+        <nav className="hidden md:block">
+          <Menu />
+        </nav>
       </div>
     </div>
   );
 };
 
 export default Navbar;
+
+<div className="render__navbar text-black font-extrabold w-full border-b-2 border-sky-500">
+  <div className="render__links-container flex sm:flex-row flex-col  justify-between items-center p-5 mx-auto">
+    <div className="render__logo text-3xl ml2 ">
+      <h1 className="bungee uppercase"></h1>
+    </div>
+    <Menu />
+  </div>
+</div>;
